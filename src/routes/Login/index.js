abf5199020886b7cfea8535fb5a3d583bd36f31e
hung bui <hung.bui@robustaeng.com>
2022-09-30 17:54:59 +0700

    cm

--- src/routes/Login/index.js
+++ src/routes/Login/index.js
@@ -1,5 +1,6 @@
-import {Button, Form, Input, DatePicker, Select} from 'antd';
-import {FacebookOutlined, GoogleOutlined} from '@ant-design/icons';
+import { useNavigate } from "react-router-dom";
+import {Button, Form, Input, Divider, notification, Space } from 'antd';
+import {FacebookOutlined, GoogleOutlined, UserOutlined, LockOutlined} from '@ant-design/icons';
 import { useEffect } from 'react';
 import moment from 'moment';
 
@@ -14,86 +15,131 @@ const formItemLayout = {
 
 const Login = () => {
   const [form] = Form.useForm();
+  const navigate = useNavigate();
   
-  // useEffect(() => {
-  //   if(rowSelected.id) {      
-  //     form.setFieldsValue({
-  //       ten: rowSelected.ten,
-  //       ngay_sinh: rowSelected.ngay_sinh ? moment(rowSelected.ngay_sinh) : null,
-  //       dia_chi: rowSelected.dia_chi,
-  //       chuc_vu: rowSelected.chuc_vu,
-  //     });
-  //   }
-  // }, [rowSelected]);
+  useEffect(() => {        
+    window.sessionStorage.removeItem("user");
+  }, []);
 
-  const onFinish = () => {
+  const onFinish = (values) => {
+    if(values.tai_khoan !== 'admin' && values.mat_khau !== 'admin') {
+      notification.warning({
+        message: 'Thông báo',
+        description: 'Tài khoản hoặc mật khẩu không đúng'
+      });
+      return;
+    }
 
+    notification.warning({
+      message: 'Thông báo',
+      description: 'Đăng nhập thành công'
+    });
+
+    redirect({
+      name: 'Administrator',
+      token: 123
+    });
+  };
+
+  const redirect = (data) => {
+    sessionStorage.setItem("user", JSON.stringify(data));
+
+    navigate('/');
   };
 
   const onFinishFailed = () => {
 
   };
 
+  const loginWithFB = () => {
+    window.FB.getLoginStatus(function(response) {   
+        if (response.status !== 'connected') {
+          return window.FB.login((res) => {
+            console.log(res)    
+            if (res.status === 'not_authorized') {              
+              return;
+            }
+            const accessToken = res.authResponse.accessToken;
+            handleToken(accessToken);
+          }, {scope: 'email'})
+        }
+
+        const accessToken = response.authResponse.accessToken;
+        handleToken(accessToken);
+    });
+  };
+
+  const handleToken = (accessToken) => {
+    window.FB.api('/me', function(response) {      
+      redirect({
+        name: response.name,
+        token: accessToken
+      });
+    });
+  };
+
   return (
-    <div className="grid justify-items-center">
-      <div className="rounded-2xl" style={{position: 'absolute', top: '30%'}}>
-        <Form
-          name="login"
-          form={form}
-          {...formItemLayout}      
-          onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
-          autoComplete="off"
-          layout="vertical"
-        >
-          <Form.Item
-            label={<b>Tài khoản</b>}
-            name="user"
-            style={{marginBottom: '5px'}}
-            rules={[
-              {
-                required: true,
-                message: 'Vui lòng nhập tên đăng nhập',
-              },
-            ]}
+    <div className='h-screen gh-login-bg'>
+      <div className="grid justify-items-center" >
+        <div className="rounded-2xl mx-4 px-8 py-20 gh-login-bg-item" style={{position: 'absolute', top: '25%'}}>
+          <Form           
+            style={{width: '300px'}} 
+            name="login"
+            form={form}
+            // {...formItemLayout}      
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+            layout="vertical"
           >
-            <Input />
-          </Form.Item>
+            <Form.Item
+              // label={<b>Tài khoản</b>}
+              name="tai_khoan"
+              style={{marginBottom: '20px'}}
+              rules={[
+                {
+                  required: true,
+                  message: 'Vui lòng nhập tên đăng nhập',
+                },
+              ]}
+            >
+              <Input prefix={<UserOutlined />} className='gh-ip' placeHolder='Tài khoản'/>
+            </Form.Item>
 
-          <Form.Item            
-            label={<b>Mật khẩu</b>}
-            name="password"
-            style={{marginBottom: '10px'}}
-            rules={[
-              {
-                required: true,
-                message: 'Vui lòng nhập mật khẩu',
-              },
-            ]}
-          >
-            <Input type="password"/>
-          </Form.Item>        
+            <Form.Item            
+              // label={<b>Mật khẩu</b>}
+              name="mat_khau"
+              style={{marginBottom: '20px'}}
+              rules={[
+                {
+                  required: true,
+                  message: 'Vui lòng nhập mật khẩu',
+                },
+              ]}
+            >
+              <Input prefix={<LockOutlined />} className='gh-ip' placeHolder='Mật khẩu' type="password"/>
+            </Form.Item>        
 
-          <Form.Item
-            // label=" "
-            colon={false}
-            style={{marginBottom: '0px'}}
-            
-          >
-            <Button htmlType="submit" style={{width: '100%'}} type="primary">Đăng nhập</Button>                  
-          </Form.Item>
-          <Form.Item
-            label=" "
-            colon={false}
-            style={{marginBottom: '0px'}}
-            
-          >
-            <Button className='gh-fb-bg' htmlType="submit" style={{width: '100%'}} type="primary" icon={<FacebookOutlined style={{fontSize: '20px'}}/>}>Đăng nhập với Facebook</Button>
-            <Button className='gh-gg-bg mt-2' htmlType="submit" style={{width: '100%'}} type="primary" icon={<GoogleOutlined style={{fontSize: '20px'}}/>}>Đăng nhập với Google</Button>
-          </Form.Item>
-        </Form>
-      </div>
-    </div>        
+            <Form.Item            
+              colon={false}
+              style={{marginBottom: '0px'}}
+              
+            >
+              <Button className='gh-btn' htmlType="submit" style={{width: '100%'}} type="primary"><span className='tracking-wider'>Đăng nhập</span></Button>                  
+            </Form.Item>
+          </Form>
+
+          <Divider />          
+
+          <div className="text-center">
+            <Space size={[20]}>
+              <Button onClick={() => loginWithFB()} className='gh-fb-bg gh-btn' type="primary" ><FacebookOutlined style={{fontSize: '20px'}}/></Button>
+              <Button className='gh-gg-bg gh-btn' type="primary" ><GoogleOutlined style={{fontSize: '20px'}}/></Button>
+            </Space>
+          </div>
+        </div>
+      </div>     
+    </div>   
   );
 };
 
